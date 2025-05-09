7b2e938ac7a323e87020aae73aa5ca0e3d06ab6a
Murilo <161628769+Murilo1006@users.noreply.github.com>
2025-05-09 18:24:25 -0300

    Add files via upload

---
+++ aula09/exemplo_lampada.php
@@ -0,0 +1,19 @@
+<?php
+    include_once 'Lampada.class.php';
+
+    $lamp1 = new Lampada();
+
+    // var_dump($lamp1);
+
+    $lamp1-> Fabricante = "Philips";
+    $lamp1-> Tenso = 110;
+    $lamp1-> Potencia = 50;
+    $lamp1-> Cor = "Branca";
+    // $lamp1-> Status = "";
+
+    echo "<br>";
+
+    $lamp1-> MostrarDados();
+    $lamp1-> Desligar();
+    $lamp1-> MostrarDados();
+?>
